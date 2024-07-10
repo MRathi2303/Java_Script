054886f434247f40667ee0a463724ea2f5034028
Moon Rathi <Moonrathi111@gmail.com>
2024-07-10 14:28:58 +0530

    git setup

---
+++ basics/variables.js
@@ -0,0 +1,18 @@
+const accountId = 230305
+let accountEmail="abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "noida"
+let accountState;
+/*
+const are not allowed to change
+accountId =2
+var keyword is not used
+ */
+
+accountEmail="mnq@gmail.com"
+accountPassword="21312321"
+accountCity = "new delhi"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
