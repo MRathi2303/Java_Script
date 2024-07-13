57c069373d387d6acadfa76a390e44f0b5ffeb6d
Moon Rathi <Moonrathi111@gmail.com>
2024-07-13 10:28:27 +0530

    1_edit

--- basics/variables.js
+++ basics/variables.js
@@ -1,5 +1,6 @@
 const accountId = 230305
 let accountEmail="abc@gmail.com"
+var name="rahul"
 var accountPassword = "12345"
 accountCity = "noida"
 let accountState;
@@ -8,11 +9,11 @@ const are not allowed to change
 accountId =2
 var keyword is not used
  */
-
+console.log(name)
 accountEmail="mnq@gmail.com"
 accountPassword="21312321"
 accountCity = "new delhi"
 
 console.log(accountId);
 
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
