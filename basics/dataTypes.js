859acc6b0ebfbfbd830e71c66c8f3e51875c6398
Moon Rathi <147794854+MRathi2303@users.noreply.github.com>
2024-07-15 17:15:16 +0530

    datatypes basics

---
+++ basics/dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+
+let name = "Moon"
+/*
+-> -> data types <- <- 
+
+number =>2 ^ 53
+bigint
+string => ""
+null => standalone value
+undefined => default value (value not assigned)
+symbol => unique
+
+*/
+
+console.log(typeof name); // give the type of data used in this case its string 
+console.log(typeof null);// the type of null is object
